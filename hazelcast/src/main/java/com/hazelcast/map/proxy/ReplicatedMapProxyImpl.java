d29ec65da28eef234583ff0c34cad8b444bc1767
Noctarius <me@noctarius.com>
2013-10-13 18:06:59 +0200

    First prototype of ReplicatedMap implementation

--- hazelcast/src/main/java/com/hazelcast/map/proxy/ReplicatedMapProxyImpl.java
+++ hazelcast/src/main/java/com/hazelcast/map/proxy/ReplicatedMapProxyImpl.java
@@ -19,24 +19,25 @@ package com.hazelcast.map.proxy;
  */
 
 import com.hazelcast.config.DistributionStrategyConfig;
+import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.EntryView;
 import com.hazelcast.core.IReplicatedMap;
-import com.hazelcast.map.EntryProcessor;
-import com.hazelcast.map.MapService;
-import com.hazelcast.map.RecordStore;
+import com.hazelcast.core.PartitioningStrategy;
+import com.hazelcast.map.*;
 import com.hazelcast.map.record.Record;
 import com.hazelcast.nio.Address;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.partition.PartitionService;
 import com.hazelcast.partition.PartitionView;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.spi.NodeEngine;
+import com.hazelcast.spi.Operation;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -51,12 +52,21 @@ public class ReplicatedMapProxyImpl<K, V>
 
     private final NodeEngine nodeEngine;
 
+    private final InMemoryFormat inMemoryFormat;
+
     public ReplicatedMapProxyImpl(String name, MapService service, NodeEngine nodeEngine) {
         super(name, service, nodeEngine);
-        MapConfig config = service.getMapContainer(name).getMapConfig();
-        this.distributionStrategyConfig = config.getDistributionStrategyConfig();
+        this.distributionStrategyConfig = mapConfig.getDistributionStrategyConfig();
         this.partitionService = nodeEngine.getPartitionService();
         this.nodeEngine = nodeEngine;
+        this.inMemoryFormat = mapConfig.getInMemoryFormat();
+        if (isEventuallyConsistent()) {
+            Operation op = new InitiallyFillReplicatedMapOperation(partitionStrategy);
+            op.setServiceName(MapService.SERVICE_NAME);
+            op.setNodeEngine(nodeEngine);
+            op.setService(service);
+            nodeEngine.getOperationService().executeOperation(op);
+        }
     }
 
     @Override
@@ -68,7 +78,7 @@ public class ReplicatedMapProxyImpl<K, V>
 
     @Override
     public Map<K, V> getAll(Set<K> keys) {
-        if (distributionStrategyConfig == DistributionStrategyConfig.Distributed) {
+        if (isEventuallyConsistent()) {
             Map<Data, K> ks = new HashMap<Data, K>(keys.size());
             for (K key : keys) {
                 if (key == null) {
@@ -90,7 +100,7 @@ public class ReplicatedMapProxyImpl<K, V>
 
     @Override
     public Future<V> getAsync(final K k) {
-        if (distributionStrategyConfig == DistributionStrategyConfig.Distributed) {
+        if (isEventuallyConsistent()) {
             return new Future<V>() {
                 private volatile boolean cancelled = false;
 
@@ -128,62 +138,216 @@ public class ReplicatedMapProxyImpl<K, V>
 
     @Override
     public EntryView<K, V> getEntryView(K key) {
-        return super.getEntryView(key);    //To change body of overridden methods use File | Settings | File Templates.
+        if (key == null) {
+            throw new NullPointerException(NULL_KEY_IS_NOT_ALLOWED);
+        }
+        Data kayData = getService().toData(key, partitionStrategy);
+        int partitionId = partitionService.getPartitionId(kayData);
+        V value = getValueInternal(key, kayData, partitionId);
+        if (value == null) {
+            return null;
+        }
+        SimpleEntryView<K, V> entryView = new SimpleEntryView<K, V>();
+        entryView.setKey(key);
+        entryView.setValue(value);
+        return entryView;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return super.entrySet();    //To change body of overridden methods use File | Settings | File Templates.
+        if (isEventuallyConsistent()) {
+            Set<Entry<K, V>> entrySet = new HashSet<Entry<K, V>>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Entry<Data, Object> entry : recordStore.entrySetObject()) {
+                        K key = (K) getService().toObject(entry.getKey());
+                        entrySet.add(new AbstractMap.SimpleImmutableEntry<K, V>(key, (V) entry.getValue()));
+                    }
+                }
+            }
+            return entrySet;
+        }
+        return super.entrySet();
     }
 
     @Override
     public Set<Entry<K, V>> entrySet(Predicate predicate) {
-        return super.entrySet(predicate);    //To change body of overridden methods use File | Settings | File Templates.
+        if (predicate == null) {
+            throw new NullPointerException("Predicate should not be null!");
+        }
+        if (isEventuallyConsistent()) {
+            Set<Entry<K, V>> entrySet = new HashSet<Entry<K, V>>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Entry<Data, Object> entry : recordStore.entrySetObject()) {
+                        K key = (K) getService().toObject(entry.getKey());
+                        Entry<K, V> e = new AbstractMap.SimpleImmutableEntry<K, V>(key, (V) entry.getValue());
+                        //TODO Is this enough or does it need an local only implementation of query?
+                        if (predicate.apply(e)) {
+                            entrySet.add(e);
+                        }
+                    }
+                }
+            }
+            return entrySet;
+        }
+        return super.entrySet(predicate);
     }
 
     @Override
     public boolean containsKey(Object k) {
-        return super.containsKey(k);    //To change body of overridden methods use File | Settings | File Templates.
+        if (k == null) {
+            throw new NullPointerException(NULL_KEY_IS_NOT_ALLOWED);
+        }
+        if (isEventuallyConsistent()) {
+            Data keyData = getService().toData(k, partitionStrategy);
+            int partitionId = partitionService.getPartitionId(keyData);
+            RecordStore recordStore = getService().getRecordStore(partitionId, name);
+            return recordStore.containsKey(keyData);
+        }
+        return super.containsKey(k);
     }
 
     @Override
     public boolean containsValue(Object v) {
-        return super.containsValue(v);    //To change body of overridden methods use File | Settings | File Templates.
+        if (v == null) {
+            throw new NullPointerException(NULL_VALUE_IS_NOT_ALLOWED);
+        }
+        if (isEventuallyConsistent()) {
+            Data valueData = getService().toData(v);
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    if(recordStore.containsValue(valueData)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return super.containsValue(v);
     }
 
     @Override
     public Object executeOnKey(K key, EntryProcessor entryProcessor) {
-        return super.executeOnKey(key, entryProcessor);    //To change body of overridden methods use File | Settings | File Templates.
+        return super.executeOnKey(key, entryProcessor);    //TODO change body of overridden methods use File | Settings | File Templates.
     }
 
     @Override
     public Set<K> keySet() {
-        return super.keySet();    //To change body of overridden methods use File | Settings | File Templates.
+        if (isEventuallyConsistent()) {
+            Set<K> keySet = new HashSet<K>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Data dataKey : recordStore.keySet()) {
+                        K key = (K) getService().toObject(dataKey);
+                        keySet.add(key);
+                    }
+                }
+            }
+            return keySet;
+        }
+        return super.keySet();
     }
 
     @Override
     public Set<K> keySet(Predicate predicate) {
-        return super.keySet(predicate);    //To change body of overridden methods use File | Settings | File Templates.
+        if (predicate == null) {
+            throw new NullPointerException("Predicate should not be null!");
+        }
+        if (isEventuallyConsistent()) {
+            Set<K> keySet = new HashSet<K>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Entry<Data, Object> entry : recordStore.entrySetObject()) {
+                        K key = (K) getService().toObject(entry.getKey());
+                        Entry<K, V> e = new AbstractMap.SimpleImmutableEntry<K, V>(key, (V) entry.getValue());
+                        //TODO Is this enough or does it need an local only implementation of query?
+                        if (predicate.apply(e)) {
+                            keySet.add(key);
+                        }
+                    }
+                }
+            }
+            return keySet;
+        }
+        return super.keySet(predicate);
     }
 
     @Override
     public Set<K> localKeySet() {
-        return super.localKeySet();    //To change body of overridden methods use File | Settings | File Templates.
+        if (isEventuallyConsistent()) {
+            return keySet();
+        }
+        return super.localKeySet();
     }
 
     @Override
     public Set<K> localKeySet(Predicate predicate) {
-        return super.localKeySet(predicate);    //To change body of overridden methods use File | Settings | File Templates.
+        if (isEventuallyConsistent()) {
+            return keySet(predicate);
+        }
+        return super.localKeySet(predicate);
     }
 
     @Override
     public Collection<V> values() {
-        return super.values();    //To change body of overridden methods use File | Settings | File Templates.
+        if (isEventuallyConsistent()) {
+            List<V> values = new ArrayList<V>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Object value : recordStore.valuesObject()) {
+                        values.add((V) value);
+                    }
+                }
+            }
+            return values;
+        }
+        return super.values();
     }
 
     @Override
     public Collection<V> values(Predicate predicate) {
-        return super.values(predicate);    //To change body of overridden methods use File | Settings | File Templates.
+        if (predicate == null) {
+            throw new NullPointerException("Predicate should not be null!");
+        }
+        if (isEventuallyConsistent()) {
+            List<V> values = new ArrayList<V>();
+            int partitionCount = partitionService.getPartitionCount();
+            for (int i = 0; i < partitionCount; i++) {
+                PartitionContainer partitionContainer = getService().getPartitionContainer(i);
+                if (partitionContainer != null) {
+                    RecordStore recordStore = partitionContainer.getRecordStore(name);
+                    for (Entry<Data, Object> entry : recordStore.entrySetObject()) {
+                        K key = (K) getService().toObject(entry.getKey());
+                        Entry<K, V> e = new AbstractMap.SimpleImmutableEntry<K, V>(key, (V) entry.getValue());
+                        //TODO Is this enough or does it need an local only implementation of query?
+                        if (predicate.apply(e)) {
+                            values.add((V) entry.getValue());
+                        }
+                    }
+                }
+            }
+            return values;
+        }
+        return super.values(predicate);
     }
 
     @Override
@@ -215,10 +379,10 @@ public class ReplicatedMapProxyImpl<K, V>
         return null;
     }
 
-    protected void storeToLocalRecordStore(Data keyData, V value, int partitionId) {
+    protected void storeToLocalRecordStore(Data keyData, Object value, int partitionId) {
         RecordStore recordStore = getService().getRecordStore(partitionId, name);
         int ttl = mapConfig.getTimeToLiveSeconds();
-        recordStore.put(keyData, getService().toData(value), getTimeInMillis(ttl, TimeUnit.SECONDS));
+        recordStore.put(keyData, convertValue(value), getTimeInMillis(ttl, TimeUnit.SECONDS));
     }
 
     protected V getValueInternal(K key, Data keyData, int partitionId) {
@@ -230,7 +394,7 @@ public class ReplicatedMapProxyImpl<K, V>
         if (value != null) {
             return value;
         }
-        if (distributionStrategyConfig == DistributionStrategyConfig.Replicated) {
+        if (!isEventuallyConsistent()) {
             value = super.get(key);
             if (value != null) {
                 storeToLocalRecordStore(keyData, value, partitionId);
@@ -239,4 +403,65 @@ public class ReplicatedMapProxyImpl<K, V>
         return null;
     }
 
+    private Set<Entry<K, V>> getFullEntrySetInternal() {
+        return super.entrySet();
+    }
+
+    private Object convertValue(Object value) {
+        if (inMemoryFormat == InMemoryFormat.OBJECT) {
+            if (value instanceof Data) {
+                return getService().toObject(value);
+            }
+            return value;
+        }
+        return getService().toData(value);
+    }
+
+    private class InitiallyFillReplicatedMapOperation extends Operation {
+        private final PartitioningStrategy<K> partitioningStrategy;
+
+        public InitiallyFillReplicatedMapOperation(PartitioningStrategy<K> partitioningStrategy) {
+            this.partitioningStrategy = partitioningStrategy;
+        }
+
+        @Override
+        public void beforeRun() throws Exception {
+        }
+
+        @Override
+        public void run() throws Exception {
+            Set<Entry<K, V>> entrySet = getFullEntrySetInternal();
+            MapService mapService = getService();
+            for (Entry<K, V> entry : entrySet) {
+                Data keyData = mapService.toData(entry.getKey(), partitioningStrategy);
+                Object value = convertValue(entry.getValue());
+                int partitionId = partitionService.getPartitionId(keyData);
+                storeToLocalRecordStore(keyData, value, partitionId);
+            }
+        }
+
+        @Override
+        public void afterRun() throws Exception {
+        }
+
+        @Override
+        public boolean returnsResponse() {
+            return false;
+        }
+
+        @Override
+        public Object getResponse() {
+            return null;
+        }
+
+        @Override
+        protected void writeInternal(ObjectDataOutput out) throws IOException {
+            throw new UnsupportedOperationException("InitiallyFillReplicatedMapOperation cannot be serialized");
+        }
+
+        @Override
+        protected void readInternal(ObjectDataInput in) throws IOException {
+            throw new UnsupportedOperationException("InitiallyFillReplicatedMapOperation cannot be serialized");
+        }
+    }
 }
